902a02c9d698ce4dad4f3b27fc3c434fdb96883c
Geovana Fernandes <gfcdm10@gmail.com>
2025-06-25 21:56:00 -0300

    fdgfd

---
+++ IaPioniers/Views/Turmas/Index.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace IaPioniers.Views.Turmas
+{
+    public class IndexModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
